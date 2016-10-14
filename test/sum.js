4dd48ec87e0b69db27043f3bced7827d8ad1aebe
Ilya Dovban <criyology@gmail.com>
2016-10-14 17:35:26 +0300

    1st bunch of sum tests

--- test/sum.js
+++ test/sum.js
@@ -1,1 +1,12 @@
 var sum = require('../sum.js');
+var expect = require('chai').expect;
+
+describe('sum', () => {
+    it('sums all its arguments', () => {
+        expect( +sum(1,2,3) ).to.equal(6);
+    });
+
+    it('can chain two calls', () => {
+        expect( +sum(1,2,3)(4) ).to.equal(10);
+    });
+})
