12d985ffc38b048563e1f626e5982c506713786a
Ilya Dovban <criyology@gmail.com>
2016-10-14 18:01:01 +0300

    2nd bunch of sum tests

--- test/sum.js
+++ test/sum.js
@@ -9,4 +9,20 @@ describe('sum', () => {
     it('can chain two calls', () => {
         expect( +sum(1,2,3)(4) ).to.equal(10);
     });
+
+    it('can chain indefinitely', () => {
+        expect( +sum(3)(7)(11) ).to.equal(21);
+    });
+
+    it('accepts other sum() calls', () => {
+        expect( +sum(1, sum(2))(sum(2)) ).to.equal(5);
+    });
+
+    it('supports empty arguments', () => {
+        expect( +sum(1)()(8) ).to.equal(9);
+    });
+
+    it('it handles negative numbers', () => {
+        expect( +sum(1,2,3)(-4,1) ).to.equal(3);
+    });
 })
