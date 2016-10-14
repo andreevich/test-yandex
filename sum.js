9e31dffae46d7f1de3a36477aff3325a2ec03d7e
andreevich <test@test.test>
2016-10-14 18:01:02 +0300

    Pass 2 tests

--- sum.js
+++ sum.js
@@ -2,7 +2,26 @@ function sum(...arg){
 	var tempSum = arg.reduce((sum, el)=>{
 		return sum+=el;
 	},0)
+	
+
+
+function tempSum_(el){
+	
+	tempSum+=el;
+
 	return tempSum;
 }
 
+tempSum_.valueOf = function (){
+		return tempSum;
+	}
+
+return tempSum_;
+
+}
+
+
+//console.log(+sum(1,2,3)(4));
+// fun = sum(1,2,3)
+// fun.
 module.exports = sum;
